a10eb2cf73904ff8818e34e3a2b8985e8cf48521
Yashpreet Singh <ypsingh199@gmail.com>
2024-09-06 19:20:40 -0400

    Create 875_Koko Eating Bananas.py

---
+++ 875_Koko Eating Bananas.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        piles.sort()
+        minimum=1 #piles.index(min(piles))
+        maximum=piles[len(piles)-1] #piles.index(max(piles))
+
+
+        while minimum < maximum :
+            k= minimum + (maximum-minimum)//2
+        
+            if sum([ceil(i/k) for i in piles])<=h:
+                #works 
+                maximum=k #we found our first value now find minimum
+            else:
+                minimum= k+1
+            
+                
+        return minimum
+
+
