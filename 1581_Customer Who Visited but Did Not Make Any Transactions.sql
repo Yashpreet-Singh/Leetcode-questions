1dcff8cf8bde66a6ba9b182af401148109ca2468
Yashpreet Singh <ypsingh199@gmail.com>
2024-09-15 20:24:44 -0400

    Update 1581_Customer Who Visited but Did Not Make Any Transactions.sql

--- 1581_Customer Who Visited but Did Not Make Any Transactions.sql
+++ 1581_Customer Who Visited but Did Not Make Any Transactions.sql
@@ -7,3 +7,12 @@ where (customer_id,visit_id) not in
 from visits v
 join transactions t using(visit_id))
 group by customer_id
+
+  -----------------------------------------------------using left join
+
+SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans
+FROM Visits AS v
+LEFT JOIN Transactions AS t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
