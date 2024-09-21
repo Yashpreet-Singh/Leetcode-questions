062df6de39ad614832cab962daa1682f44311fe3
Yashpreet Singh <ypsingh199@gmail.com>
2024-09-21 15:32:23 -0400

    Update 572_Subtree of Another Tree.py

--- 572_Subtree of Another Tree.py
+++ 572_Subtree of Another Tree.py
@@ -27,4 +27,27 @@ class Solution:
 
         return (self.isSubtree(root.left,subRoot)) or (self.isSubtree(root.right,subRoot))
 
+#-----------------------------------------Preorder traversal for graphs---------------- time: O(m*n)
 
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        string_s = self.traverse_tree(s)
+        string_t = self.traverse_tree(t)
+        if string_t in string_s:
+            return True
+        return False
+    
+    
+    def traverse_tree(self, s):
+        if s:
+            return f"#{s.val} {self.traverse_tree(s.left)} {self.traverse_tree(s.right)}"
+        return None
+
+
+'''
+You use it as a delimiter between the nodes in the string representation.
+
+This is because: take a test case of [12], [2]. So we have our root with val=12 and no children, and subRoot node with val=2 and no children. If we turn them to string without the #, we get the following root="12 None None" and subRoot ="2 None None". This results in a false positive since the string "2 None None" is a substring of "12 None None". If we add a # to the beginning of every node with a value, we get root="#12 None None" and subRoot="#2 None None". Now, subRoot's string is no longer a substring of root's string and doesn't create a false positive anymore.
+
+In fact, we can replace the # character with basically any letter (e.g. h) since all it needs to do is delimit (but we can't use a number as a delimiter!)
+'''
